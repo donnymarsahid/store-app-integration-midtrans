0eecdb77ec17b63a20bad07d37880cb6805c2d50
unknown <donnymrshd.94@gmail.com>
2021-08-20 01:01:20 +0700

    updates detailpage

--- src/users/Users.js
+++ src/users/Users.js
@@ -3,6 +3,7 @@ import './css/style.css';
 import imgHeader from '../assets/img/img-header.png';
 import { context } from '../App';
 import CardCoffe from './cardsProducts/CardCoffe';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
   const { coffeeVariant } = useContext(context);
@@ -18,7 +19,9 @@ const Users = () => {
             <h1 class="mb-3">WAYSBUCKS</h1>
             <p>Things are changing, but we’re still here for you</p>
             <p>We have temporarily closed our in-store cafes, but select grocery and drive-thru locations remaining open. Waysbucks Drivers is also available</p>
-            <button class="btn-order">LETS ORDER</button>
+            <Link to="/all-menu">
+              <button class="btn-order">LETS ORDER</button>
+            </Link>
           </div>
           <div class="img-header">
             <img src={imgHeader} alt="header-waysbucks" />
